397c7008475704c4759cb758057ce32983a163e3
abdallah <Abdallah@local.com>
2024-01-25 17:41:53 +0200

    abdallah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  const [text, setText] = React.useState("");
+  const [checked, setChecked] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const textChange = (e) => {
+    if (checked) setChecked(false);
+    setText(e.target.value);
+  };
+  const check = async () => {
+    setIsLoading(true);
+    const url = "https://cwg.sahehly.com/Diac/Sahehly";
+
+    const payload = {
+      "g-flag": true,
+      type: "SSahahlyWebSite",
+      word: text,
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Basic V2ViU2FoZWhseTo4dVc1c2FkN2dGRzJC",
+    };
+    const response = await axios.post(url, payload, { headers });
+    const data = response.data;
+    setText(data.diacWord);
+    if (data) {
+      setIsLoading(false);
+      setChecked(true);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? (
+        <h1>Is Loading ................</h1>
+      ) : (
+        <div>
+          <h3> -من فضلك ادخل النص -</h3>
+          <textarea
+            onChange={textChange}
+            rows={10}
+            style={{ width: "600px", fontSize: "20px", fontWeight: "bold" }}
+          />
+          <div>
+            <button
+              style={{ width: "120px", height: "70px", margin: "10px" }}
+              onClick={check}
+              className="btn btn-success"
+            >
+              صحح
+            </button>
+          </div>
+          <h3
+            style={{
+              fontWeight: "22px",
+              color: checked ? "green" : "",
+            }}
+          >
+            {text}
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
